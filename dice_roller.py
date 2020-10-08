c7937cf3345b1f6a0353c2202134afeb2b1d3691
Juliana <juhnicacio.bio@gmail.com>
2020-10-08 10:58:25 +0200

    Added a maximum of dice to roll

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,11 @@
 def main():
   import random
   dice_size = int(input('How many sides are the dice?'))
-  dice_rolls = int(input('How many dice would you like to roll?'))
+  dice_rolls = int(input('How many dice would you like to roll: 1, 2 or 3? \n'))
   dice_sum = 0
+  while dice_rolls < 1 or dice_rolls > 3:
+    print( "Invalid range. You can roll only 1, 2 or 3 dice. Choose again.")
+    dice_rolls = int(input('How many dice would you like to roll: 1, 2 or 3? \n'))
   for i in range(0, dice_rolls):
     roll = random.randint(1,dice_size)
     dice_sum = dice_sum + roll
@@ -14,7 +17,7 @@ def main():
     else:
       print(f'You rolled a {roll}')
     #print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+  print(f'You have rolled a total of {dice_sum} \n')
 
 if __name__== "__main__":
   main()
